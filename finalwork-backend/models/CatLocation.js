cfb9250c4d45a2d24954a9e1c29ccbfb5320fd18
Rowanbiets <rowan.biets@student.ehb.be>
2025-06-12 22:16:12 +0200

    feat(add): adding jump

--- finalwork-backend/models/CatLocation.js
+++ finalwork-backend/models/CatLocation.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const catLocationSchema = new mongoose.Schema({
   latitude: Number,
   longitude: Number,
+  jump: Number,
   activityLevel: { type: Number },
   timestamp: { type: Date, default: Date.now },
 });
