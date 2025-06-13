075d55abc887b1f8a277d1a51cd46e7514cdd00b
Rowanbiets <rowan.biets@student.ehb.be>
2025-06-13 15:30:41 +0200

    feat(add): adding profile

--- finalwork/app/(tabs)/_layout.tsx
+++ finalwork/app/(tabs)/_layout.tsx
@@ -1,16 +1,16 @@
-import { StyleSheet, View, Dimensions, Platform, Text } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import { colors } from "@/constants/Colors";
 
 import TabBar from "enhanced-fluid-bottom-navigation-bar";
 
-// import HomeScreen from "./index";
 import DashboardScreen from ".";
 import MapScreen from "./map";
 import ActivityScreen from "./activity";
-import Header from "@/components/ui/Header";
 import SettingsScreen from "./settings";
+import UserProfileScreen from "./profile"; // jouw nieuwe profiel scherm
+import Header from "@/components/ui/Header";
 
 const Tab = createBottomTabNavigator();
 
@@ -18,7 +18,7 @@ export default function Layout() {
   return (
     <>
       <View style={styles.backgroundWrapper}>
-        <Header></Header>
+        <Header />
       </View>
       <Tab.Navigator
         tabBar={(props) => (
@@ -67,22 +67,19 @@ export default function Layout() {
         )}
         screenOptions={{ headerShown: false }}
       >
-        {/* <Tab.Screen name="Home" component={DashboardScreen} /> */}
         <Tab.Screen name="Dashboard" component={DashboardScreen} />
         <Tab.Screen name="Activiteit" component={ActivityScreen} />
-
         <Tab.Screen name="Map" component={MapScreen} />
         <Tab.Screen name="Instellingen" component={SettingsScreen} />
+        <Tab.Screen name="Profiel" component={UserProfileScreen} />
       </Tab.Navigator>
     </>
   );
 }
+
 const styles = StyleSheet.create({
   backgroundWrapper: {
     position: "absolute",
-    // top: -150,
-    // left: 0,
-    // right: 0,
     width: 50,
     backgroundColor: "red",
   },
